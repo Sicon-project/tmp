4c8f0ef561111dfeff16d3f0a030c6ca3fd7518e
djjd2000 <djjd2000@naver.com>
2022-05-24 12:03:19 +0900

    유효 동작만 angle 인식

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import time
+import json
 
 # 손 2개 인식
 max_num_hands = 2
@@ -34,66 +35,80 @@ label = labelFile.astype( np.float32)
 knn = cv2.ml.KNearest_create()
 knn.train(angle,cv2.ml.ROW_SAMPLE,label)
 
-for i in range(0, 1):
-    cap = cv2.VideoCapture('videos/ml2.mp4')
-
+for i in range(1, 30):
+    # cap = cv2.VideoCapture('videos/ml2.mp4')
+    cap = cv2.VideoCapture('data/Front/NIA_SL_SEN00%02d_REAL01_F.mp4'%i)
+    file = open('morpheme/Front/NIA_SL_SEN00%02d_REAL01_F_morpheme.json'%i, 'r', encoding='UTF8')
+    jsonStr = json.load(file)
+    start = jsonStr["data"][0]['start']
+    end = jsonStr["data"][0]['end']
+    #인접한 프레임 계산
+    print(start, end)
     startTime = time.time()
     prev_index = 0
     sentence = ''
     recognizeDelay = 1
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     print(length)
-    while length:
+    for tmp in range(0, length):
+    # while length:
         ret, img = cap.read()
-        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        result = hands.process(imgRGB)
-        if result.multi_hand_landmarks is not None:
-            for res in result.multi_hand_landmarks:
-                joint = np.zeros((21,3))
-                for j, lm in enumerate(res. landmark):
-                    joint[j] = [lm.x, lm.y, lm.z]
+        if(img is None):
+            break        
+        if(tmp < start*30 - 1):
+            continue
+        elif(tmp > end*30 +1):
+            continue
+        else:
+            imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            result = hands.process(imgRGB)
+            if result.multi_hand_landmarks is not None:
+                for res in result.multi_hand_landmarks:
+                    joint = np.zeros((21,3))
+                    for j, lm in enumerate(res. landmark):
+                        joint[j] = [lm.x, lm.y, lm.z]
 
-                v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0,17,18,19],:]
-                v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20],:]
+                    v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0,17,18,19],:]
+                    v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20],:]
 
-                v = v2 - v1
-                v = v/np.linalg.norm(v, axis=1)[:,np.newaxis]
-                comparev1 = v[[0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17],:]
-                comparev2 = v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19],:]
-                angle = np.arccos(np.einsum('nt,nt->n', comparev1, comparev2))
+                    v = v2 - v1
+                    v = v/np.linalg.norm(v, axis=1)[:,np.newaxis]
+                    comparev1 = v[[0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17],:]
+                    comparev2 = v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19],:]
+                    angle = np.arccos(np.einsum('nt,nt->n', comparev1, comparev2))
 
-                angle = np.degrees(angle)
-                for num in angle:
-                    num = round(num, 6)
-                    f.write(str(num))
-                    f.write(',')
-                f.write("27.000000") #라벨을 가변 필드로 바꿔야 함
-                f.write('\n')
-                data = np.array([angle],dtype=np.float32)
-                ret, results, neighbours, dist = knn.findNearest(data,3)
-                index = int(results[0][0])
-                if index in gesture.keys():
-                    if index != prev_index:
-                        startTime = time.time()
-                        prev_index = index
-                    else:
-                        if time.time() - startTime > recognizeDelay:
-                            if index == 26:
-                                sentence += ''
-                            elif index == 27:
-                                sentence = ''
-                            else:
-                                sentence += gesture[index]
+                    angle = np.degrees(angle)
+                    for num in angle:
+                        num = round(num, 6)
+                        f.write(str(num))
+                        f.write(',')
+                    f.write("27.000000") #라벨을 가변 필드로 바꿔야 함
+                    f.write('\n')
+                    data = np.array([angle],dtype=np.float32)
+                    ret, results, neighbours, dist = knn.findNearest(data,3)
+                    index = int(results[0][0])
+                    if index in gesture.keys():
+                        if index != prev_index:
                             startTime = time.time()
+                            prev_index = index
+                        else:
+                            if time.time() - startTime > recognizeDelay:
+                                if index == 26:
+                                    sentence += ''
+                                elif index == 27:
+                                    sentence = ''
+                                else:
+                                    sentence += gesture[index]
+                                startTime = time.time()
 
-                    cv2.putText(img, gesture[index]. upper(),(int(res. landmark[0].x * img.shape[1] - 10),
-                                int(res.landmark[0].y * img.shape[0] + 40)),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),3)
+                        cv2.putText(img, gesture[index]. upper(),(int(res. landmark[0].x * img.shape[1] - 10),
+                                    int(res.landmark[0].y * img.shape[0] + 40)),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),3)
 
-                mp_drawing.draw_landmarks(img,res,mp_hands.HAND_CONNECTIONS)
-        cv2.putText(img, sentence, (20,440),cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255),3)
+                    mp_drawing.draw_landmarks(img,res,mp_hands.HAND_CONNECTIONS)
+            cv2.putText(img, sentence, (20,440),cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255),3)
 
-        cv2.imshow('HandTracking', img)
-        cv2.waitKey(1)
+            cv2.imshow('HandTracking', img)
+            cv2.waitKey(1)
 
 
 f.close();
